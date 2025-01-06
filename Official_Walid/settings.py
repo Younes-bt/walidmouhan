bad6cb434e7a743ed3137aa5cfc3dd5f40515f08
younes El bettate <bt.younesse@gmail.com>
2025-01-06 23:12:07 +0100

    security - Add Additional HTTP security headers.

--- Official_Walid/settings.py
+++ Official_Walid/settings.py
@@ -160,6 +160,13 @@ cloudinary.config(
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 try:
     from .settings_local import *
 except:
